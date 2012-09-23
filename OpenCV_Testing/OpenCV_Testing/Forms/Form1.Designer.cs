b1bb841814eb768332f4111b043dbcf51320dac8
Brandon West <ZenithWest@aol.com>
2012-09-22 23:28:21 -0500

    OpenCV_Testing Haar Cascade

---
+++ OpenCV_Testing/OpenCV_Testing/Forms/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace OpenCV_Testing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pictureBox1.Location = new System.Drawing.Point(99, 64);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(444, 410);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(336, 13);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(80, 17);
+			this.checkBox1.TabIndex = 1;
+			this.checkBox1.Text = "checkBox1";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Location = new System.Drawing.Point(336, 41);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(80, 17);
+			this.checkBox2.TabIndex = 2;
+			this.checkBox2.Text = "checkBox2";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.Location = new System.Drawing.Point(226, 13);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(80, 17);
+			this.checkBox3.TabIndex = 3;
+			this.checkBox3.Text = "checkBox3";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(627, 522);
+			this.Controls.Add(this.checkBox3);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+		  private System.Windows.Forms.PictureBox pictureBox1;
+		  private System.Windows.Forms.CheckBox checkBox1;
+		  private System.Windows.Forms.CheckBox checkBox2;
+		  private System.Windows.Forms.CheckBox checkBox3;
+		  private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ OpenCV_Testing/OpenCV_Testing/Forms/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+
+namespace OpenCV_Testing
+{
+	public partial class Form1 : Form
+	{
+		private Capture cap;
+		private HaarCascade haarRightEye;
+		private HaarCascade haarLeftEye;
+		private HaarCascade haarMouth;
+		private HaarCascade haarFace;
+		private HaarCascade haarNose;
+		private HaarCascade haarRightEar;
+		private HaarCascade haarLeftEar;
+		private HaarCascade haarUpperBody;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			checkBox2.Checked = true;
+			using (Emgu.CV.Image<Bgr,byte> nextFrame = cap.QueryFrame())
+			{
+				checkBox2.Checked = true;
+				if (nextFrame != null)
+				{
+					// there's only one channel (greyscale), hence the zero index
+					//var faces = nextFrame.DetectHaarCascade(haar)[0];
+					checkBox3.Checked = true;
+
+					Emgu.CV.Image<Gray, byte> grayframe = nextFrame.Convert<Gray, byte>();
+					var faces =
+						grayframe.DetectHaarCascade(
+							haarFace, 1.4, 4,
+						HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+						new Size(nextFrame.Width / 8, nextFrame.Height / 8)
+						)[0];
+					var rightear =
+						grayframe.DetectHaarCascade(
+							haarRightEar, 1.4, 4,
+						HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+						new Size(nextFrame.Width / 8, nextFrame.Height / 8)
+						)[0];
+					var leftear =
+						grayframe.DetectHaarCascade(
+							haarLeftEar, 1.4, 4,
+						HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+						new Size(nextFrame.Width / 8, nextFrame.Height / 8)
+						)[0];
+					var righteye =
+						grayframe.DetectHaarCascade(
+							haarRightEye, 1.4, 4,
+						HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+						new Size(nextFrame.Width / 8, nextFrame.Height / 8)
+						)[0];
+					var lefteye =
+						grayframe.DetectHaarCascade(
+							haarLeftEye, 1.4, 4,
+						HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+						new Size(nextFrame.Width / 8, nextFrame.Height / 8)
+						)[0];
+					var mouth =
+						grayframe.DetectHaarCascade(
+							haarMouth, 1.4, 4,
+						HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+						new Size(nextFrame.Width / 8, nextFrame.Height / 8)
+						)[0];
+					var nose =
+						grayframe.DetectHaarCascade(
+							haarNose, 1.4, 4,
+						HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+						new Size(nextFrame.Width / 8, nextFrame.Height / 8)
+						)[0];
+					var upperbody =
+						grayframe.DetectHaarCascade(
+							haarUpperBody, 1.4, 4,
+						HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+						new Size(nextFrame.Width / 8, nextFrame.Height / 8)
+						)[0];
+
+					foreach (var face in faces)
+					{
+						nextFrame.Draw(face.rect, new Bgr(Color.AliceBlue), 1);
+					}
+
+					foreach (var face in rightear)
+					{
+						nextFrame.Draw(face.rect, new Bgr(Color.Red), 1);
+					}
+
+					foreach (var face in leftear)
+					{
+						nextFrame.Draw(face.rect, new Bgr(Color.Blue), 1);
+					}
+
+					foreach (var face in righteye)
+					{
+						nextFrame.Draw(face.rect, new Bgr(Color.Green), 1);
+					}
+
+					foreach (var face in lefteye)
+					{
+						nextFrame.Draw(face.rect, new Bgr(Color.Yellow), 1);
+					}
+
+					foreach (var face in mouth)
+					{
+						nextFrame.Draw(face.rect, new Bgr(Color.Purple), 1);
+					}
+
+					foreach (var face in nose)
+					{
+						nextFrame.Draw(face.rect, new Bgr(Color.Orange), 1);
+					}
+
+					foreach (var face in upperbody)
+					{
+						nextFrame.Draw(face.rect, new Bgr(Color.GreenYellow), 1);
+					}
+
+					pictureBox1.Image = nextFrame.ToBitmap();
+
+				}
+			}
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			// passing 0 gets zeroth webcam
+
+			cap = new Capture(5);
+			// adjust path to find your xml
+
+			haarRightEye = new HaarCascade("C:\\OpenCV\\OpenCV\\data\\haarcascades\\haarcascade_mcs_righteye.xml");
+			haarLeftEye = new HaarCascade("C:\\OpenCV\\OpenCV\\data\\haarcascades\\haarcascade_mcs_lefteye.xml");
+			haarMouth = new HaarCascade("C:\\OpenCV\\OpenCV\\data\\haarcascades\\haarcascade_mcs_mouth.xml");
+			haarNose = new HaarCascade("C:\\OpenCV\\OpenCV\\data\\haarcascades\\haarcascade_mcs_nose.xml");
+			haarRightEar = new HaarCascade("C:\\OpenCV\\OpenCV\\data\\haarcascades\\haarcascade_mcs_rightear.xml");
+			haarLeftEar = new HaarCascade("C:\\OpenCV\\OpenCV\\data\\haarcascades\\haarcascade_mcs_leftear.xml");
+			haarUpperBody = new HaarCascade("C:\\OpenCV\\OpenCV\\data\\haarcascades\\haarcascade_upperbody.xml");
+			haarFace = new HaarCascade("C:\\OpenCV\\OpenCV\\data\\haarcascades\\haarcascade_frontalface_alt.xml");
+			//	 "..\\..\\..\\..\\lib\\haarcascade_frontalface_alt2.xml");
+			checkBox1.Checked = true;
+		}
+	}
+}
